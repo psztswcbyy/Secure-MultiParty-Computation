8aafc056c63e787d2b27f723f218e3a49afbdcfe
sz peng <1946282924@qq.com>
2023-12-28 17:59:38 +0800

    add test accuracy and effientcy directory

--- S2PHMP/RandInputMatrix.m
+++
@@ -1,6 +0,0 @@
-function M = RandInputMatrix(N,FirstNumMin,FirstNumMax,minEp,maxEp)
-Ori_M = rand(N,'double') + randi([FirstNumMin,FirstNumMax],N);
-Exp_M = 10.^(randi([minEp, maxEp],N));
-Sign_M = sign(2*rand(N)-1);
-M = Sign_M.*Ori_M.*Exp_M;
-end
